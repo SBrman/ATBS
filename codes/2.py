c98aebaef1621bc05b7d775eaadf1e1149c7417d
SBrman <simanta9693@gmail.com>
2022-05-18 10:16:31 -0500

    Delete 2.py

--- codes/2.py
+++
@@ -1,8 +0,0 @@
-name = input('Enter your name: ')
-age = int(input('Enter your age: '))
-
-if name == 'Alice':
-    if age < 12:
-        print('You are not Alice, kiddo')
-    else:
-        print('Hi, Alice.')
